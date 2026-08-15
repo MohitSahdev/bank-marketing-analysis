096373c8e27b88f6363c1f3bcf0ea098d419b3c4
Mohit Sahdev <msa343@uclive.ac.nz>
2026-08-16 00:13:12 +0530

    Add basic data exploration

---
+++ Analysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df=pd.read_csv(r"C:\Users\hp\Desktop\Mohit Sahdev - Data Scientist\bank-marketing-analysis\Data\bank-direct-marketing-campaigns.csv")
+#basic understanding
+#print(df.head()) #data header
+#print(df.shape) #how many rows and columns
+#print(df.columns) #name of columns
+#print(df.info) #data type and missing values
+#print(df.describe) #numberical data in statistics
+
