b95957219de5ec8242cc4429a6912ec91d25d6e0
Mohit Sahdev <msa343@uclive.ac.nz>
2026-08-16 01:04:24 +0530

    add data quality check

--- Analysis.py
+++ Analysis.py
@@ -7,3 +7,19 @@ df=pd.read_csv(r"C:\Users\hp\Desktop\Mohit Sahdev - Data Scientist\bank-marketin
 #print(df.info) #data type and missing values
 #print(df.describe) #numberical data in statistics
 
+#data quality
+#print(df.isnull().sum()) #check missing values
+#print(df.duplicated().sum()) # check duplicate row
+#print(df.nunique()) #check unique values
+#checking value count of individually columns
+print(df["job"].value_counts())
+print(df["age"].value_counts())
+print(df["marital"].value_counts())
+print(df["education"].value_counts())
+print(df["default"].value_counts())
+print(df["housing"].value_counts())
+print(df["loan"].value_counts())
+print(df["contact"].value_counts())
+print(df["month"].value_counts())
+print(df["poutcome"].value_counts())
+print(df["y"].value_counts())
